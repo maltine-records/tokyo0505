9de03309d707371dc5215c781f352c72dd64b449
yuiseki <yuiseki@gmail.com>
2014-04-16 18:33:45 +0900

    吹き出し出すところまで

---
+++ tokyo0505/TimetableAnnotaion.h
@@ -0,0 +1,13 @@
+//
+//  TimetableAnnotaion.h
+//  tokyo0505
+//
+//  Created by cerevo on 2014/04/16.
+//  Copyright (c) 2014年 Nubot, inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TimetableAnnotaion : NSObject
+
+@end
---
+++ tokyo0505/TimetableAnnotaion.m
@@ -0,0 +1,13 @@
+//
+//  TimetableAnnotaion.m
+//  tokyo0505
+//
+//  Created by cerevo on 2014/04/16.
+//  Copyright (c) 2014年 Nubot, inc. All rights reserved.
+//
+
+#import "TimetableAnnotaion.h"
+
+@implementation TimetableAnnotaion
+
+@end
